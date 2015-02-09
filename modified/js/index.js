b475cbe3939f74acf0f76ca59a4e541be1a7c64d
Mindrose <mindrose@localhost.localdomain>
2015-02-09 04:43:24 -0500

    modified added

---
+++ modified/js/index.js
@@ -0,0 +1,58 @@
+
+
+$(document).ready(function(){
+    $(".submenu").hide();
+    $("#menu").click(function(){
+        $(".submenu").show();
+    });
+});
+
+$(document).ready(function(){
+    $(".menu1").hide();
+    $("#b1").click(function(){
+        $(".menu2").hide();
+        $(".menu3").hide();
+        $(".menu4").hide();
+        $(".menu1").show();
+    });
+});
+
+$(document).ready(function(){
+    $(".menu1").hide();
+    $("#b2").click(function(){
+        //$(".menu1").replaceWith();
+        $(".menu1").hide();
+        $(".menu3").hide();
+        $(".menu4").hide();
+        $(".menu2").show();
+    });
+});
+
+
+$(document).ready(function(){
+    $(".menu2").hide();
+    $("#b3").click(function(){
+        $(".menu2").hide();
+        $(".menu1").hide();
+        $(".menu4").hide();
+        $(".menu3").show();
+    });
+});
+
+
+$(document).ready(function(){
+    $(".menu3").hide();
+    $("#b4").click(function(){
+        $(".menu2").hide();
+        $(".menu3").hide();
+        $(".menu1").hide();
+        $(".menu4").show();
+    });
+});
+
+$(document).ready(function(){
+    $(".menu4").hide();});
+
+$("#menu12").click(function(){
+   $(".dropdown-menu").css("visibility","visible");
+});
